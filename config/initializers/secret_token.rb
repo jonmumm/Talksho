5f022444acbcc878515cd8aeaf7c609e0c86e515
Jonathan Mumm <mumm@tokbox.com>
2011-10-25 10:01:07 +0800

    Project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talkshow::Application.config.secret_token = '8f136cf08025ec2442efe8334380537307e240940c8525785d9296d437d39bffcc8f7a57341a9ba418d98f13b13acc74ef1fa32ad7f416576c2d2b0e67393945'
