43604c236abbfaa06d4bbdb9c20afea99e0d52ab
Jonathan Mumm <mumm@tokbox.com>
2011-10-25 13:31:06 +0800

    Added host controller

---
+++ app/controllers/host_controller.rb
@@ -0,0 +1,5 @@
+class HostController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Talkshow::Application.routes.draw do
+  get "host" => 'host#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
