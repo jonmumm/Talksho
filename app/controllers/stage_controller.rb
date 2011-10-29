1762cce92e29e28bb9a8c6f8d88707f213b1b024
Jonathan Mumm <mumm@tokbox.com>
2011-10-29 14:52:15 -0700

    Added stage state controller

--- app/controllers/stage_controller.rb
+++ app/controllers/stage_controller.rb
@@ -1,11 +1,33 @@
 class StageController < ApplicationController
 
   def get
-    render :json => params
+    stage = Stage.where(:sessionId => params[:sessionId]).select("state, streamId")
+
+    render :json => stage
   end
 
   def set
-    render :json => params
+    # Only do if adminstrator
+    if not session[params[:sessionId]]
+      response = { :error => "User does not have privileges to perform this action" }
+    else
+      stage = Stage.where(:sessionId => params[:sessionId], :state => params[:state]).first
+
+      if response
+        stage.streamId = params[:streamId]
+        stage.save
+      else
+        stage = Stage.new
+        stage.sessionId = params[:sessionId]
+        stage.streamId = params[:streamId]
+        stage.state = params[:state]
+        stage.save
+      end
+
+      response = stage
+    end
+    
+    render :json => response
   end
 
 end
